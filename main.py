40734f73d6c6a892fa9e5685930144b746bda46d
snaipLOL1 <166751167+snaipLOL1@users.noreply.github.com>
2024-07-17 20:09:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def beats(self, other):
         return other == Choice.paper
 
 
-class Game 
+class Game: 
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
